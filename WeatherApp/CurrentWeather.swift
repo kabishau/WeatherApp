704bc94131830b9a5d96f52f863dbaf6ba2db7d3
Aleksey Kabishau <aliaksei.kabishau@gmail.com>
2018-03-07 21:38:20 -0500

    additional file for managing Weather Icon and iboutlets

---
+++ WeatherApp/CurrentWeather.swift
@@ -0,0 +1,12 @@
+import Foundation
+import UIKit
+
+struct CurrentWeather {
+    
+    let temperature: Double
+    let appearentTemperature: Double
+    let humidity: Double
+    let pressure: Double
+    let icon: UIImage
+    
+}
