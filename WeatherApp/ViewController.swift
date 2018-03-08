3979f98fc15e86c42f8d2ab0a68d4fc8f7a9cce2
Aleksey Kabishau <aliaksei.kabishau@gmail.com>
2018-03-07 22:21:58 -0500

    update UI with fake data

--- WeatherApp/ViewController.swift
+++ WeatherApp/ViewController.swift
@@ -13,5 +13,24 @@ class ViewController: UIViewController {
     @IBAction func refreshButtonTapped(_ sender: Any) {
     }
     
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let icon = WeatherIconManager.Rain.image
+        let currentWeather = CurrentWeather(temperature: 10, appearentTemperature: 5, humidity: 30, pressure: 750, icon: icon)
+        
+        updateUIWith(currentWeather: currentWeather)
+    }
+    
+    func updateUIWith(currentWeather: CurrentWeather) {
+        self.imageView.image = currentWeather.icon
+        self.pressureLabel.text = "\(Int(currentWeather.pressure)) mm"
+        self.humidityLabel.text = "\(Int(currentWeather.humidity)) %"
+        self.temperatureLabel.text = "\(Int(currentWeather.temperature))˚C"
+        self.appearentTemperatureLabel.text = "Feels like: \(Int(currentWeather.temperature))˚C"
+    }
+    
 }
+
+
 
