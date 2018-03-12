a7b9baeece86a71e1c6a24052598076eca1fd511
Aleksey Kabishau <aliaksei.kabishau@gmail.com>
2018-03-11 22:28:37 -0400

    typealias for custom JSONTaskWithRequest method

--- WeatherApp/APIManager.swift
+++ WeatherApp/APIManager.swift
@@ -4,10 +4,13 @@ import Foundation
 
 protocol APIManager {
     
+    typealias JSONTask = URLSessionDataTask
+    typealias JSONCompletionHandler = ([String: Any]?, HTTPURLResponse?, Error?)
+    
     var sessionConfiguration: URLSessionConfiguration { get }
     var URLSession: URLSession { get }
     
-    func JSONTaskWith(request: URLRequest, completionHandler: ([String: Any]?, HTTPURLResponse?, Error?)) -> URLSessionDataTask
+    func JSONTaskWith(request: URLRequest, completionHandler: JSONCompletionHandler) -> JSONTask
     
     // to define type of session configuration
     init(sessionConfiguration: URLSessionConfiguration)
