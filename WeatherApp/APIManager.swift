f8ee3c31d391b1e3e365987ed30e378b4893778b
user131656 <user131656@ny218.local>
2018-03-12 19:04:33 -0400

    protocol interface has been finished

--- WeatherApp/APIManager.swift
+++ WeatherApp/APIManager.swift
@@ -1,16 +1,24 @@
 import Foundation
 
+typealias JSONTask = URLSessionDataTask
+typealias JSONCompletionHandler = ([String: Any]?, HTTPURLResponse?, Error?)
+
+// generic enum, in this specific case should be type of CustomWeather
+enum APIResult<T> {
+    case Success(T)
+    case Failure(Error)
+}
+
 // protocol/interface for working with network
 
 protocol APIManager {
-    
-    typealias JSONTask = URLSessionDataTask
-    typealias JSONCompletionHandler = ([String: Any]?, HTTPURLResponse?, Error?)
     
     var sessionConfiguration: URLSessionConfiguration { get }
     var URLSession: URLSession { get }
     
     func JSONTaskWith(request: URLRequest, completionHandler: JSONCompletionHandler) -> JSONTask
+    // using generic
+    func fetch<T>(request: URLRequest, parse: ([String: AnyObject]) -> T?, completionHandler: (APIResult<T>) -> Void)
     
     // to define type of session configuration
     init(sessionConfiguration: URLSessionConfiguration)
