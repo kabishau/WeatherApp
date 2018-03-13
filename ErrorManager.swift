7025ceac9c764ffb822c70598b77a7353615a34d
Aleksey Kabishau <aliaksei.kabishau@gmail.com>
2018-03-12 23:35:04 -0400

    error in JSONCompletionHandler typealias has been fixed error in variable name (session) has been fixed first method in extension to APIManager protocol has been implemented ErrorManager file has been created for error descriptions

---
+++ ErrorManager.swift
@@ -0,0 +1,5 @@
+import Foundation
+
+// for sake of avoiding duplication of constants - the best practice to add 3 letters before costant
+public let SWINetworkingErrorDomain = "ru.swiftbook.WeatherApp.NetworkingError"
+public let MissingHTTPResponseError = 100
--- WeatherApp/APIManager.swift
+++ WeatherApp/APIManager.swift
@@ -1,7 +1,7 @@
 import Foundation
 
 typealias JSONTask = URLSessionDataTask
-typealias JSONCompletionHandler = ([String: Any]?, HTTPURLResponse?, Error?)
+typealias JSONCompletionHandler = ([String: Any]?, HTTPURLResponse?, Error?) -> Void
 
 // generic enum, in this specific case should be type of CustomWeather
 enum APIResult<T> {
@@ -14,7 +14,7 @@ enum APIResult<T> {
 protocol APIManager {
     
     var sessionConfiguration: URLSessionConfiguration { get }
-    var URLSession: URLSession { get }
+    var session: URLSession { get }
     
     func JSONTaskWith(request: URLRequest, completionHandler: JSONCompletionHandler) -> JSONTask
     // using generic
@@ -23,4 +23,41 @@ protocol APIManager {
     // to define type of session configuration
     init(sessionConfiguration: URLSessionConfiguration)
     
+}
+
+extension APIManager {
+    func JSONTaskWith(request: URLRequest, completionHandler: @escaping JSONCompletionHandler) -> JSONTask {
+        
+        let dataTask = session.dataTask(with: request) { (data, response, error) in
+            
+            guard let HTTPResponse = response as? HTTPURLResponse else {
+                // empty array for using in NSError
+                let userInfo = [
+                    NSLocalizedDescriptionKey: NSLocalizedString("Missing HTTP Response", comment: "")
+                ]
+                let error = NSError(domain: SWINetworkingErrorDomain, code: MissingHTTPResponseError, userInfo: userInfo)
+                completionHandler(nil, nil, error)
+                return
+            }
+            
+            if data == nil {
+                if let error = error {
+                    completionHandler(nil, HTTPResponse, error)
+                }
+            } else {
+                switch HTTPResponse.statusCode {
+                case 200:
+                    do {
+                        let json = try JSONSerialization.jsonObject(with: data!, options: []) as? [String: AnyObject]
+                        completionHandler(json, HTTPResponse, nil)
+                    } catch let error as NSError {
+                        completionHandler(nil, HTTPResponse, error)
+                    }
+                default:
+                    print("We have got response status \(HTTPResponse.statusCode)")
+                }
+            }
+        }
+        return dataTask
+    }
 }
